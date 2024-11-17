432865155706363a10548918f388e2445746aab7
AkhilG <akhilgandi@unitednewdigitalmedia.com>
2024-11-17 21:09:37 +0530

    step changes

--- src/app/create-resume/components/project/project.component.ts
+++ src/app/create-resume/components/project/project.component.ts
@@ -16,12 +16,12 @@ export class ProjectComponent  implements OnInit {
   ngOnInit() {
     // Initialize form with a FormArray to hold multiple projects
     this.projectsForm = this.fb.group({
-      projects: this.fb.array([this.createProjectForm()]), // Initially, one project form
+      projects: this.fb.array([this.createprojectsForm()]), // Initially, one project form
     });
   }
 
   // Create a new project form group
-  createProjectForm(): FormGroup {
+  createprojectsForm(): FormGroup {
     return this.fb.group({
       project_title: ['', Validators.required],
       role: ['', Validators.required],
@@ -39,28 +39,9 @@ export class ProjectComponent  implements OnInit {
     return this.projectsForm.get('projects') as FormArray;
   }
 
-<<<<<<< HEAD
-  // Method to update existing project data
-  updateForm() {
-    if (this.projectForm.valid) {
-      // Construct the payload based on form values
-      const payload = {
-        project_title: this.projectForm.get('project_title')?.value,
-        role: this.projectForm.get('role')?.value,
-        institute: this.projectForm.get('institute')?.value,
-        location: this.projectForm.get('location')?.value,
-        description: this.projectForm.get('description')?.value,
-        start_date: this.projectForm.get('start_date')?.value,
-        end_date: this.projectForm.get('end_date')?.value,
-        currently_working: this.projectForm.get('currently_working')?.value, // Make sure this is a boolean (true or false)
-        stepIndex: 6 // You can replace 6 with any dynamic value if necessary
-      };
-  
-      // Call the updateProjects method and pass the payload
-=======
   // Add a new project form to the array
   addProject() {
-    this.projects.push(this.createProjectForm());
+    this.projects.push(this.createprojectsForm());
   }
 
   // Method to submit all the projects
@@ -70,7 +51,6 @@ export class ProjectComponent  implements OnInit {
       const payload = this.projectsForm.value.projects;
 
       // Call the service to update the projects
->>>>>>> 964a9d466c25b7360321363e3eafc562208fd52d
       this.projectService.updateProjects(payload).subscribe(
         (response: any) => {
           console.log('Projects updated successfully', response);
